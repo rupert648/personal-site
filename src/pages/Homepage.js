01e129fa0b4876d6dc7a7ae26ecae834052b9e55
rupert648 <47698091+rupert648@users.noreply.github.com>
2021-05-27 17:19:26 +0100

    changed picture to relevant picture and added gradient

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -19,6 +19,7 @@ import projects from '../data/projectData';
 import experience from '../data/experienceData'
 
 import "./FancyButton.scss"
+import stAndrews from '../assets/standrews.jpg'
 
 const HomePage = styled.section`
 `;
@@ -26,7 +27,7 @@ const HomePage = styled.section`
 const ImageSection = styled.div`
     width: 100%;
     height: 100vh;
-    background-image: url(https://images.unsplash.com/photo-1498550744921-75f79806b8a7?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=b0f6908fa5e81286213c7211276e6b3d&auto=format&fit=crop&w=1500&q=80);
+    background-image: linear-gradient(to bottom, rgba(0,0,0,0), rgba(0, 42, 67, 1)), url(${stAndrews});
     background-position: center top;
 	background-size:cover;    
 
